09b414d905cdf4c20bf426b18c174808e1df9a74
Luis Molinuevo <89353175+luismolinuevo@users.noreply.github.com>
2024-08-08 14:49:11 +0000

    updated demos

---
+++ JSDOM/app.js
@@ -0,0 +1,26 @@
+//A variable have 3 parts to it
+
+//First var, let, const. Const variables value can be changed later on
+
+
+//Second part is the variable name. Ex: let age
+
+//Third is setting the value. 
+//Ex
+let age = 20;
+
+let user_name = document.getElementById("name");
+user_name.innerHTML = "pape123";
+
+//Different types of variable values
+let teststring = "adlfkasdflasdlf asdlfja ;lsfja;sdlfj as;dlfjasdf"
+let testint = 1;
+let testfloat = 2.0;
+let testboolean = true;
+
+if(age >= 21) {
+    console.log("Go into the bar")
+} 
+else {
+    console.log("Cannot go")
+}
